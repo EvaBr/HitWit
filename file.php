af30a1c96138c29e2cc2942857171c1dd85e4fa9
EvaBr <eva.breznik@tum.de>
2016-01-05 22:26:20 +0100

    missing update

--- file.php
+++ file.php
@@ -5,15 +5,15 @@
   if ($in=="manual"){
     //manually
     echo 'Please enter the number of genes your dataset contains measurements for: ';
-    echo '<br> <input type="number" min="1" name="genes"> <br>';
+    echo '<br> <input type="number" min="1" name="geni" id="numGen"> <br>';
     echo '<br>Please enter your measurements, separated by either commas or spaces, e.g. in case of three samples 100.3, 150.7, 110.0 or 100.3 150.7 110.0:';
-    echo '<br> <input type="text" name="samples"> <br>';
+    echo '<br> <textarea name="meritve" id="numMeas" cols="60" rows="6"> </textarea><br>';
   } else {
     //with a file
     //echo 'in = '.$in.' <br>';
     echo 'File should contain a data matrix with one dimension standing for genes and the other one for samples.
           Fields have to be separated by tabs or white spaces, but not by commas. Please enter a valid path and file name:';
-    echo '<br> <input type="text" name="samples"> <br>';
+    echo '<br> <input type="text" name="path" id="path"> <br>';
     echo '<br>
           Does the file contain column names?<br>
                 <div class="iradio">
